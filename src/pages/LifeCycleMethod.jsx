0808e2d4b425d831477447e02afb89e8c944c9fd
fikri <me@fikri.tech>
2020-08-26 11:42:23 +0700

    json server

--- src/pages/LifeCycleMethod.jsx
+++ src/pages/LifeCycleMethod.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 class LifeCycleMethod extends Component {
-
-    componentDidMount(){
-        alert('CompDidMount')
+    state = {
+        name : ''
     }
+    // jalan setelah render pertama
+    componentDidMount(){
+        console.log('did mount jalan')
+    }
+
+    componentDidUpdate(){
+        console.log('did update jalan')
+    }
+
+    componentWillUnmount(){
+        console.log('will unmount jalan')
+    }
+
+    onH1Click = () => {
+        this.setState({name : 'isi'})
+    }
+
+
 
     // mounting => comdidmount // get data dari api
     // updating => compdidupdate // kalau ada perubahan state
@@ -12,10 +29,11 @@ class LifeCycleMethod extends Component {
 
     
     render() {
-        alert('render')
+        console.log('render jalan')
         return (
             <div>
-                <h1>Lifecycle Method</h1>
+                <h1 onClick={this.onH1Click}>Lifecycle Method</h1>
+                <p>{this.state.name}</p>
                 <p>Each component in React has a lifecycle which you can monitor and manipulate during its three main phases.
                 The three phases are: Mounting, Updating, and Unmounting.</p>
             </div>
