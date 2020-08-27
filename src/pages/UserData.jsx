18665f650c3f584d75a6dd6b711c245a96ea16f6
fikri <me@fikri.tech>
2020-08-27 10:03:32 +0700

    form input

--- src/pages/UserData.jsx
+++ src/pages/UserData.jsx
@@ -5,7 +5,8 @@ import Axios from 'axios'
 const apiUrl = 'http://localhost:2000/users'
 class UserData extends Component {
     state = {
-        data : null
+        data : null,
+        showForm : true
     }
 
     componentDidMount(){
@@ -46,8 +47,11 @@ class UserData extends Component {
         }
         return (
             <div>
-                <h1>My User Data</h1>
+                
                 <div className="container">
+                    <h1>My User Data</h1>   
+                    <input type="button" className='btn btn-primary mb-5' onClick={() => this.setState({showForm : true})} value="add new data"/>
+
                     <table className='table'>
                         <thead>
                             <tr>
@@ -64,6 +68,24 @@ class UserData extends Component {
                             }
                         </tbody>
                     </table>
+                    {
+                        this.state.showForm ? 
+                        <div className="row mt-5 justify-content-center fixed-bottom mb-5">
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <input type="text" placeholder='Enter Username' className='form-control'/>
+                                        <input type="text" placeholder='Enter Email' className='form-control my-3'/>
+                                        <input type="password" placeholder='Enter Your Password' className='form-control'/>
+                                        <input type="button" value="Submit" className='btn btn-info mt-4'/>
+                                        <input type="button" onClick={ () => this.setState({showForm : false})} value="Close Form" className='btn btn-danger mt-4'/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        null
+                    }
                 </div>
             </div>
         )
